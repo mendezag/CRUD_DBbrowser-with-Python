d76d0436aadef91f01c752369a58f9f1573cd0b9
mendezag <55924328+mendezag@users.noreply.github.com>
2020-01-15 21:59:30 -0300

    Add files via upload

---
+++ CRUD_DBbrowser with Python/index.py
@@ -0,0 +1,153 @@
+from tkinter import ttk
+from tkinter import *
+
+
+import sqlite3 #modulo de conexion
+#esta clase tiene todos los metodos de las ventanas 
+#(agregar,eliminar,editar)
+#crear botones
+class Product:
+    dbname = 'database.db'
+
+    def __init__(self, window):
+        self.wind = window
+        self.wind.title('Products Application')
+
+        #Creating a Frame Container
+        frame = LabelFrame(self.wind, text = 'Register a new Product')
+        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+
+        #Name Input
+        Label(frame, text = 'Name: ').grid(row = 1, column = 0)
+        self.name = Entry(frame)
+        self.name.focus()
+        self.name.grid(row = 1, column = 1)
+
+        #Price Input
+        Label(frame, text = 'Price: ').grid(row = 2, column = 0)
+        self.price = Entry(frame)
+        self.price.grid(row = 2, column = 1)
+
+        #Button Add Product
+        #Command gives information about the funtion has to run
+        ttk.Button(frame, text = 'Add Product',command = self.add_product).grid(row = 3, columnspan = 2, sticky = W + E)
+
+        #Output Messages
+        self.message = Label(text = '', fg = 'green')
+        self.message.grid(row = 3, column = 0, columnspan = 2, sticky = W + E)
+        
+
+        #Table
+        self.tree = ttk.Treeview(height = 10, columns =2)
+        self.tree.grid(row = 4, column = 0, columnspan = 2)
+        self.tree.heading('#0', text ='Name', anchor = CENTER)
+        self.tree.heading('#1', text ='Price', anchor = CENTER)
+
+        #DELETE & EDIT buttons
+        ttk.Button(text = 'DELETE', command = self.delete_product).grid(row = 5 , column = 0, sticky = W+E)
+        ttk.Button(text = 'EDIT', command = self.edit_product).grid(row = 5 , column = 1, sticky = W+E)
+
+        #Filling the Row
+        self.get_products()
+       
+    def run_query(self, query, parameters = ()):
+        with sqlite3.connect(self.dbname) as conn:
+            cursor = conn.cursor()
+            result = cursor.execute(query, parameters)
+            conn.commit()
+        return result
+    
+    def get_products(self):
+        #Cleaning table
+        records = self.tree.get_children()
+        for element in records:
+            self.tree.delete(element)
+            
+        #Quering data
+        query = 'SELECT * FROM Product ORDER BY name DESC'
+        db_rows = self.run_query(query)
+        #Filling data
+        for row in db_rows:
+            self.tree.insert('',0, text = row[1], values = row[2])
+
+    def validation (self):
+        return len(self.name.get())!=0 and len (self.price.get())!=0
+
+
+    def add_product(self): 
+        if self.validation():
+            query = 'INSERT INTO product VALUES(NULL,?,?)'
+            parameters = (self.name.get(),self.price.get())
+            #print('Data Saved!')
+            self.run_query(query,parameters) #Run run_query with parameters received
+            self.message['text']= 'Product {} added successfully!'.format(self.name.get())
+            self.name.delete(0, END)
+            self.price.delete(0, END)
+            
+        else:
+            
+            self.message['text']= 'Name & Price are required'
+        self.get_products()
+
+    def delete_product(self):
+        #print(self.tree.item(self.tree.selection))
+        self.message['text']=''
+        try:
+            self.tree.item(self.tree.selection())['text'][0] #HD or Laptop,etc.
+        except IndexError as e :
+            self.message['text'] = 'Please Select a Record'   
+            return  
+        self.message['text']=''
+        name = self.tree.item(self.tree.selection())['text']
+        query = 'DELETE FROM product WHERE name = ?'
+        self.run_query(query,(name,))
+        self.message['text']= 'Product {} has been deleted'.format(name)
+        self.get_products()
+    
+    def edit_product(self):
+        self.message['text']= ''
+        try:
+            self.tree.item(self.tree.selection())['text'][0] #HD or Laptop,etc.
+        except IndexError as e :
+            self.message['text'] = 'Please Select a Record'
+            return
+        name = self.tree.item(self.tree.selection())['text']
+        old_price = self.tree.item(self.tree.selection())['values'][0]
+        self.edit_wind = Toplevel()
+        self.edit_wind.title = 'Edit Tittle'
+
+        #Old Name
+        Label(self.edit_wind, text = 'Old Name: ').grid(row= 0, column= 1)
+        Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value = name),state = 'readonly').grid(row=0, column = 2)
+        #New Name
+        Label(self.edit_wind, text = 'New Name').grid(row = 1, column = 1)
+        new_name = Entry(self.edit_wind)
+        new_name.grid(row = 1, column = 2)
+
+        #Old Price
+        Label(self.edit_wind, text = 'Old Price: ').grid(row= 2, column= 1)
+        Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value = old_price),state = 'readonly').grid(row=2, column = 2)
+        
+        #New Price
+        Label(self.edit_wind, text = 'New Price').grid(row = 3, column = 1)
+        new_price = Entry(self.edit_wind)
+        new_price.grid(row = 3, column = 2)
+
+        Button(self.edit_wind, text = 'Update', command = lambda: self.edit_records(new_name.get(),name,new_price.get(),old_price)).grid(row=4, column=2, sticky = W)
+
+    def edit_records(self,new_name,name,new_price,old_price):
+        query= 'UPDATE product SET name = ? , price= ? WHERE name=? AND price=?'
+        parameters = (new_name, new_price, name, old_price)
+        self.run_query(query, parameters)
+        self.edit_wind.destroy()
+        self.message['text'] = 'Record {} updated ok!'.format(name)
+        self.get_products()
+
+if __name__== '__main__':
+    window = Tk()
+    application = Product(window)
+    window.geometry('400x400')
+    window.resizable(0,0)
+    window.resizable (width=None, height=None)
+    window.configure (background = 'royalblue4')
+    window.mainloop()
